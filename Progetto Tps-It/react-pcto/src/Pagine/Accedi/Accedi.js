1e527dfd66aa55ab272674ff4c507f1c648a2a5f
Bissty23 <manuel.fedeli237@gmail.com>
2022-03-12 21:17:47 +0100

    Modifiche alla pagina di Accedi.js

--- Progetto Tps-It/react-pcto/src/Pagine/Accedi/Accedi.js
+++ Progetto Tps-It/react-pcto/src/Pagine/Accedi/Accedi.js
@@ -7,55 +7,55 @@ import './Accedi.css'
 class Accedi extends React.Component {
 
   render() {
-    
-      return(
-        <div className="container mt-4">
-          <div class="alert alert-danger" role="alert" id="c">
-            A simple warning alert—check it out!
-            <button onClick={() => ($('.alert').hide())} type="button" class="btn-close" aria-label="Close"></button>
-          </div>
 
-          <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Username o Email" id="InUsername"/>   
-          </div>
-          <div className="input-group mb-3">
-            <input type="password" className="form-control" placeholder="password" id="InPassword"/>   
-          </div>
-          
-          <input onClick={() => this.GetAccount($('#InUsername').val(), $('#InPassword').val())} type="button" id="btnAccedi" className="btn btn-dark" value="Accedi"/>
+    return(
+      <div className="container mt-4">
+        
+        <div class="alert alert-danger" role="alert">
+          <a id="errore"/>
+          <button onClick={() => ($('.alert').hide())} type="button" class="btn-close"></button>
+        </div>    
+
+        <div className="input-group mb-3">
+          <input type="text" className="form-control" placeholder="Username o Email" id="InUsername"/>   
         </div>
-      );
+        <div className="input-group mb-3">
+          <input type="password" className="form-control" placeholder="password" id="InPassword"/>   
+        </div>
+        
+        <input onClick={() => this.GetAccount($('#InUsername').val(), $('#InPassword').val())} type="button" id="btnAccedi" className="btn btn-dark" value="Accedi"/>
+      </div>
+    );
   }
 
   GetAccount = (username, password) =>{ 
     $('#btnAccedi').prop('disabled', true);
 
-    CryptoJS.SHA3(password)
-
     Axios.get("http://localhost:8090/api/Account/" + username).then(
       (risposta) =>{
 
-        localStorage.setItem('accesso', true)
-        localStorage.setItem('username', risposta.data[0].Username)
-        localStorage.setItem('password', risposta.data[0].Password)
-        localStorage.setItem('numeroditelefono', risposta.data[0].Username)
-        localStorage.setItem('ruolo', risposta.data[0].Ruolo)
-        localStorage.setItem('email', risposta.data[0].Email)
-        localStorage.setItem('Sede', risposta.data[0].Sede)
+        if(CryptoJS.SHA3(password) === risposta.data[0].Password){
+          alert('brumotti gay')
+        }
+        else {
+          this.Errore("la Password inserita non è corretta")
+          $('#InPassword').val('')
+        }
 
-        window.location.href = '/';
       },
-      (error) =>{ 
+      (errore) =>{ 
+
+        if(errore.toString().includes("Network Error")) {
+          $('#errore').html("Errore: Connessione al DataBase non riuscita ")
+        }
+        else if (errore.toString().includes("Request failed with status code 404")){
+          $('#errore').html("Errore: Username o Email inserita non correttamente")
+        }
+
         $('.alert').show()
         $('#btnAccedi').prop('disabled', false)
       })
   }
-
-
-  Controlli = () =>{
-
-  }
-    
 }
- 
+
 export default Accedi;
